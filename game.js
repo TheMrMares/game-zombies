b4c64af79971b114cc44a81d57215d1bcc598d9a
Unknown <rak.bartosz98@gmail.com>
2018-06-01 09:28:57 +0200

    Game loop, project structure

---
+++ game.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var myCanvas = document.getElementById('myCanvas');
+    var ctx = myCanvas.getContext('2d');
+
+    var gameInterval = window.setInterval(game, 1000/15);
+});
+
+// Game
+function game(){
+    console.log('xd');
+}
