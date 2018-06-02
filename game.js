a2ff55ab68c08d4009f1b16741bc689512d7bc13
Unknown <rak.bartosz98@gmail.com>
2018-06-02 12:51:44 +0200

    Colissions system improvements
    Rebuilt colissions system

--- game.js
+++ game.js
@@ -20,23 +20,23 @@ function init(){
 function game(){
     //Calculate
     myPlayer.vy += myBackground.gravity;
-    myPlayer.x += myPlayer.vx*5;
-    myPlayer.y += myPlayer.vy*5;
-    if(myPlayer.x < 0){
-        myPlayer.x = 0;
+    myPlayer.x1 += myPlayer.vx*5;
+    myPlayer.y1 += myPlayer.vy*5;
+    if(myPlayer.x1 < 0){
+        myPlayer.x1 = 0;
     }
-    if(myPlayer.x > myBackground.w){
-        myPlayer.x = myBackground.w;
+    if(myPlayer.x1 > myBackground.w){
+        myPlayer.x1 = myBackground.w;
     }
-    if(myPlayer.y < 0){
-        myPlayer.y = 0;
+    if(myPlayer.y1 < 0){
+        myPlayer.y1 = 0;
     }
-    if(myPlayer.y > myBackground.h - myPlayer.h){
-        myPlayer.y = myBackground.h - myPlayer.h
+    if(myPlayer.y1 > myBackground.h - myPlayer.h){
+        myPlayer.y1 = myBackground.h - myPlayer.h
     }
 
-    myBackground.x = 0-myPlayer.x + myCanvas.width/2 - myPlayer.w/2;
-    myBackground.y = 0-myPlayer.y + myCanvas.height/2 - myPlayer.h/2;
+    myBackground.x = 0-myPlayer.x1 + myCanvas.width/2 - myPlayer.w/2;
+    myBackground.y = 0-myPlayer.y1 + myCanvas.height/2 - myPlayer.h/2;
 
     //Draw
     ctx.fillStyle = 'grey';
@@ -45,35 +45,34 @@ function game(){
     ctx.drawImage(myBackground.res, myBackground.x, myBackground.y);
 
     myBackground.boxes.forEach(function(item, index){
-        if((myPlayer.x >= item.x && myPlayer.x <= item.x + item.w) || (myPlayer.x + myPlayer.w >= item.x && myPlayer.x + myPlayer.w <= item.x + item.w) && (myPlayer.oldy + myPlayer.h < item.y || myPlayer.oldy > item.y + item.h)){
-            if(myPlayer.y + myPlayer.h >= item.y && myPlayer.y + myPlayer.h < item.y + item.h){
-                myPlayer.y = item.y - myPlayer.h - 1;
-                myPlayer.vy = 0;
-            }
-            if(myPlayer.y <= item.y + item.h && myPlayer.y > item.y){
-                myPlayer.y = item.y + item.h + 1;
-                myPlayer.vy = 0;
-            }
-        }
-        if((myPlayer.y > item.y && myPlayer.y <= item.y + item.h) || (myPlayer.y + myPlayer.h > item.y && myPlayer.y + myPlayer.h < item.y + item.h) || ((item.y > myPlayer.y && item.y < myPlayer.y + myPlayer.h) || (item.y + item.h >= myPlayer.y && item.y + item.h <= myPlayer.y + myPlayer.h))){
-            if(myPlayer.x + myPlayer.w >= item.x && myPlayer.x + myPlayer.w <item.x + item.w){
-                myPlayer.x = item.x - myPlayer.w -1;
-                myPlayer.vx = 0;
-            }
-            if(myPlayer.x <= item.x + item.w && myPlayer.x > item.x){
-                myPlayer.x = item.x + item.w + 1;
-                myPlayer.vx = 0;
-            }
-        }
         
+        if(Math.abs(myPlayer.x1 - item.x1) <= item.w){
+            if(myPlayer.y2 >= item.y1 && myPlayer.y2 < item.y2 && myPlayer.vy>= 0){
+                myPlayer.vy = 0;
+                myPlayer.y1 = item.y1 - myPlayer.h;
+            }
+            if(myPlayer.y1 <= item.y2 && myPlayer.y1 > item.y1){
+                myPlayer.y1 = item.y2;
+                myPlayer.vy = 0;
+            }
+        }
+        if(Math.abs(myPlayer.y1 - item.y1) <= item.h){
+            if(myPlayer.x2 >= item.x1 && myPlayer.x2 < item.x2 && myPlayer.vx >= 0){
+                myPlayer.x1 = item.x1 - myPlayer.w - 5;
+            }
+            if(myPlayer.x1 <= item.x2 && myPlayer.x1 > item.x1 && myPlayer.vx <= 0){
+                myPlayer.x1 = item.x2 + 5;
+            }
+        }
 
         ctx.fillStyle = 'orange';
-        ctx.fillRect(myBackground.x + item.x,myBackground.y + item.y,item.w,item.h);
+        ctx.fillRect(myBackground.x + item.x1,myBackground.y + item.y1,item.w,item.h);
     });
 
     ctx.fillStyle = 'red';
     ctx.fillRect(myCanvas.width/2 - myPlayer.w/2,myCanvas.height/2 - myPlayer.h/2,myPlayer.w,myPlayer.h);
-    myPlayer.oldy = myPlayer.y;
+    myPlayer.saveCoordinates();
+    //console.log('#X: '+myPlayer.x1+' #Y: '+myPlayer.y1+ ' #oldX: '+myPlayer.oldx+' #oldY: '+myPlayer.oldy);
 }
 
 //Objects
@@ -84,23 +83,34 @@ function Background(x,y, w, h, res){
     this.y = y;
     this.res = res
     this.gravity = 0.2;
-    this.boxes = [new Box(300,380,20,50),new Box(400,450,20,50),new Box(600,470,20,20)];
+    this.boxes = [new Box(200,380,20,50),new Box(400,450,20,50),new Box(600,470,20,20)];
 }
 
 function Player(x, y, w, h) {
     this.w = w;
     this.h = h;
-    this.x = x;
-    this.y = y;
+    this.x1 = x;
+    this.y1 = y;
+    this.x2 = x + w;
+    this.y2 = y + h;
     this.vx = 0;
     this.vy = 0;
     this.oldy = 0;
+    this.olx = 0;
+    this.saveCoordinates = function() {
+        this.x2 = this.x1 + this.w;
+        this.y2 = this.y1 + this.h;
+        this.oldy = this.y1;
+        this.oldx = this.x1;
+    }
 }
 function Box(x,y,w,h){
     this.w = w;
     this.h = h;
-    this.x = x;
-    this.y = y;
+    this.x1 = x;
+    this.y1 = y;
+    this.x2 = x + w;
+    this.y2 = y + h;
 }
 
 //Functions
